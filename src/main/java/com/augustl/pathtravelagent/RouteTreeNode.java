c5b05366f0eeab4ff7fecb1b3884f55e4306890d
August Lilleaas <august@augustl.com>
2014-08-08 16:54:55 +0200

    Ensuring pathSegmentChildNodes is never mutated.

--- src/main/java/com/augustl/pathtravelagent/RouteTreeNode.java
+++ src/main/java/com/augustl/pathtravelagent/RouteTreeNode.java
@@ -2,14 +2,14 @@ package com.augustl.pathtravelagent;
 
 import com.augustl.pathtravelagent.segment.IParametricSegment;
 
-import java.util.HashMap;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RouteTreeNode<T_REQ extends IRequest, T_RES> {
     private final String label;
     private final IRouteHandler<T_REQ, T_RES> handler;
-    private final HashMap<String, RouteTreeNode<T_REQ, T_RES>> pathSegmentChildNodes;
+    private final Map<String, RouteTreeNode<T_REQ, T_RES>> pathSegmentChildNodes;
     private final ParametricChild<T_REQ, T_RES> parametricChild;
     private final RouteTreeNode<T_REQ, T_RES> wildcardChild;
 
@@ -21,7 +21,7 @@ public class RouteTreeNode<T_REQ extends IRequest, T_RES> {
         RouteTreeNode<T_REQ, T_RES> wildcardChild) {
         this.label = label;
         this.handler = handler;
-        this.pathSegmentChildNodes = pathSegmentChildNodes;
+        this.pathSegmentChildNodes = Collections.unmodifiableMap(pathSegmentChildNodes);
         this.parametricChild = parametricChild;
         this.wildcardChild = wildcardChild;
     }
